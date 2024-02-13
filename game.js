104ce1232e07cb7bb3b810c622213477f0313414
Abdel Yasser <zahabys@gmail.com>
2024-02-12 19:03:21 -0700

    Added auto play

--- game.js
+++ game.js
@@ -31,6 +31,22 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     playerMoveHistory = [0, 0, 0];
   }
 
+  let isAutoPlaying = false;
+  let intervalId;
+
+  function autoPlay() {
+    if (!isAutoPlaying){
+      intervalId = setInterval(function() {
+        const playerMove = pickComputerMove();
+        playGame(playerMove);
+      }, 1000);
+      isAutoPlaying = true;
+    } else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+    }
+  }
+
   function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
